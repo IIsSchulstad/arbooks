2128e74e4b629e73a5cbdf52a7cac278578c81a0
Rasmus <rallea95@gmailcom>
2019-11-01 15:19:50 +0100

    Created books view

---
+++ arbooks/arbooks/BooksViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BooksViewController.swift
+//  arbooks
+//
+//  Created by Rasmus Albrektsen on 01/11/2019.
+//  Copyright © 2019 Rasmus Stamm. All rights reserved.
+//
+
+import UIKit
+
+class BooksViewController: UIViewController {
+
+    
+    @IBOutlet weak var bookImage: UIImageView!
+    @IBOutlet weak var bookTitle: UILabel!
+    @IBOutlet weak var bookEdition: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}

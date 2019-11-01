d1344fcc17dbc8edc67037316de8f7e9c10e8a8f
Rasmus <rallea95@gmailcom>
2019-11-01 14:44:00 +0100

    Created settings view

---
+++ arbooks/arbooks/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  arbooks
+//
+//  Created by Rasmus Albrektsen on 01/11/2019.
+//  Copyright © 2019 Rasmus Stamm. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}

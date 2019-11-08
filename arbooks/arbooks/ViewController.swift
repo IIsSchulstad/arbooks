2fd57a1390a142e43f2975935c61aecb7e617a84
Rasmus <rallea95@gmailcom>
2019-11-08 15:26:03 +0100

    made the folders again because i was right the first time

--- arbooks/arbooks/ViewController.swift
+++ arbooks/arbooks/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import ARKit
 
 class ViewController: UIViewController {
-
+   
     @IBOutlet weak var sceneView: ARSCNView!
     let config = ARWorldTrackingConfiguration()
     override func viewDidLoad() {

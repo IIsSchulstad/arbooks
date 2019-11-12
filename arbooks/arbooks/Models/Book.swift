8034b3685c011982060ba642b9d6beb166ccd74b
Rasmus <rallea95@gmailcom>
2019-11-12 19:54:26 +0100

    First implementation

---
+++ arbooks/arbooks/Models/Book.swift
@@ -0,0 +1,19 @@
+//
+//  Book.swift
+//  arbooks
+//
+//  Created by Rasmus Albrektsen on 12/11/2019.
+//  Copyright © 2019 Rasmus Stamm. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+    
+    var title: String;
+    var author: String;
+    var yearPublished: Int;
+    
+    
+}
+
